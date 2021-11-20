ef077106423811fc5fde2b1eb6adb4c40b5ee62b
YoungWenMing <35167225+YoungWenMing@users.noreply.github.com>
2021-11-20 20:32:13 +0800

    [format] format some code. (#16)
    Co-authored-by: yangwenming <yangwenming@bytedance.com>

--- src/token.h
+++ src/token.h
@@ -8,46 +8,46 @@
 namespace Kaleidoscope {
 
 #define KEYWORD_LIST(T)                           \
-  T(DEF, "def", 0)                                \
-  T(EXTERN, "extern", 0)                          \
-  T(IF, "if", 0)                                  \
-  T(ELSE, "else", 0)                              \
-  T(FOR, "for", 0)                                \
-  T(IN, "in", 0)                                  \
-  T(UNARY, "unary", 0)                            \
-  T(BINARY, "binary", 0)                          \
-  T(VAR, "var", 0)                                \
-  T(RETURN, "return", 0)
+  T(DEF,        "def",      0)                    \
+  T(EXTERN,     "extern",   0)                    \
+  T(IF,         "if",       0)                    \
+  T(ELSE,       "else",     0)                    \
+  T(FOR,        "for",      0)                    \
+  T(IN,         "in",       0)                    \
+  T(UNARY,      "unary",    0)                    \
+  T(BINARY,     "binary",   0)                    \
+  T(VAR,        "var",      0)                    \
+  T(RETURN,     "return",   0)
 
 #define BINARY_OP_LIST(T)                         \
-  T(MUL, "*", 7)                                  \
-  T(DIV, "/", 7)                                  \
-  T(LT, "<", 3)                                   \
-  T(GT, ">", 3)                                   \
-  T(OR, "|", 2)                                   \
-  T(AND, "&", 2)                                  \
-  T(ADD, "+", 5)                                  \
-  T(SUB, "-", 5)
+  T(MUL,        "*",        7)                    \
+  T(DIV,        "/",        7)                    \
+  T(LT,         "<",        3)                    \
+  T(GT,         ">",        3)                    \
+  T(OR,         "|",        2)                    \
+  T(AND,        "&",        2)                    \
+  T(ADD,        "+",        5)                    \
+  T(SUB,        "-",        5)
  
 #define UNARY_OP_LIST(T)                          \
-  T(NOT, "!", 0)
+  T(NOT,        "!",        0)
 
 #define TOKEN_LIST(T)                             \
-  T(LPAREN, "(", 0)                               \
-  T(RPAREN, ")", 0)                               \
-  T(LBRACE, "{", 0)                               \
-  T(RBRACE, "}", 0)                               \
-  T(SEMICOLON, ";", 0)                            \
-  T(COMMA, ",", 0)                                \
-  T(ASSIGN, "=", 1)                               \
-  T(NUMBER, nullptr, 0)                           \
-  T(IDENTIFIER, nullptr, 0)                       \
+  T(LPAREN,     "(",        0)                    \
+  T(RPAREN,     ")",        0)                    \
+  T(LBRACE,     "{",        0)                    \
+  T(RBRACE,     "}",        0)                    \
+  T(SEMICOLON,  ";",        0)                    \
+  T(COMMA,      ",",        0)                    \
+  T(ASSIGN,     "=",        1)                    \
+  T(NUMBER,     nullptr,    0)                    \
+  T(IDENTIFIER, nullptr,    0)                    \
   KEYWORD_LIST(T)                                 \
   BINARY_OP_LIST(T)                               \
   UNARY_OP_LIST(T)                                \
   T(UNINITIALIZED, nullptr, 0)                    \
-  T(ILLEGAL, "ILLEGAL", 0)                        \
-  T(EOS, "EOS", 0)
+  T(ILLEGAL,    "ILLEGAL",  0)                    \
+  T(EOS,        "EOS",      0)
 
 class Token {
  public:
